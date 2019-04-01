32ab58fd524058d8a371a01ad16348116c982536
luisNovaes <luismagnovaes@gmail.com>
2019-04-01 16:12:30 -0300

    Criando operação de instanciação

--- src/main/java/com/magno/modelo/ModeloApplication.java
+++ src/main/java/com/magno/modelo/ModeloApplication.java
@@ -1,13 +1,33 @@
 package com.magno.modelo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.magno.modelo.domain.Categoria;
+import com.magno.modelo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModeloApplication {
+public class ModeloApplication implements CommandLineRunner {
+	
+	@Autowired	
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	Categoria cat1 =  new Categoria(null, "Informática");
+	Categoria cat2 =  new Categoria(null, "Escritório");
+	
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+		
 	}
 
 }
