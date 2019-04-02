4367ad4fa633469583f3254ffed558de9dde2cf5
luisNovaes <luismagnovaes@gmail.com>
2019-04-02 17:06:25 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/magno/modelo/resources/CategoriaResource.java
+++ src/main/java/com/magno/modelo/resources/CategoriaResource.java
@@ -23,8 +23,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	 
 	 @RequestMapping(value = "/{id}", method=RequestMethod.GET)  
-	 public ResponseEntity<?> find(@PathVariable Integer id) { 
-		 Categoria obj = service.buscar(id); 
+	 public ResponseEntity<Categoria> find(@PathVariable Integer id) { 
+		 Categoria obj = service.find(id); 
 		 return ResponseEntity.ok().body(obj);
 	 	}
 	 
@@ -37,7 +37,13 @@ public class CategoriaResource {
 		 
 	 }
 	 
-	 
+	 @RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	 public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id ) {
+		 obj.setId(id);
+		 obj = service.update(obj);
+		 return ResponseEntity.noContent().build();
+		 
+	 }
 	 
 	 
 	 }
