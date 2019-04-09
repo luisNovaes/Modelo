b78c097a499c38716a0da998709eec33aa2091c4
luisNovaes <luismagnovaes@gmail.com>
2019-04-09 12:43:24 -0300

    Implementando toString de Pedido

--- src/main/java/com/magno/modelo/services/PedidoService.java
+++ src/main/java/com/magno/modelo/services/PedidoService.java
@@ -30,12 +30,13 @@ public class PedidoService {
 	private PagamentoRepository pagamentoRepository;
 
 	@Autowired
-
 	private ItemPedidoRepository itemPedidoRepository;
 
 	@Autowired
-
 	private ProdutoService produtoService;
+	
+	@Autowired
+	private ClienteService clienteService; 
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -47,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -57,12 +59,13 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 
 		itemPedidoRepository.saveAll(obj.getItens());
-
+		System.out.println(obj);
 		return obj;
 	}
 
