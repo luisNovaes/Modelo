63f75e85ebc8bfa4ceb54db9747160bcd9b4cfd8
luisNovaes <luismagnovaes@gmail.com>
2019-04-09 18:58:30 -0300

    Email HTML

--- src/main/java/com/magno/modelo/services/EmailService.java
+++ src/main/java/com/magno/modelo/services/EmailService.java
@@ -1,12 +1,18 @@
 package com.magno.modelo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.magno.modelo.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
